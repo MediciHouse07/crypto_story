9222d047b6a9fd46f634e905839584f689af3a15
MediciHouse07 <wallacecorona163@gmail.com>
2022-01-10 13:26:20 +0800

    fix a bug: table doesn't display when select a country

--- app.R
+++ app.R
@@ -13,13 +13,13 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   output$table <- renderTable({
     if (input$country_ind=='All') dataset<-filter(gdp,Country!='All' & GDP_nominal<input$less_country)
-    else dataset <- filter(gdp,Country==input$country_ind & GDP_nominal_2017<input$less_country)
+    else dataset <- filter(gdp,Country==input$country_ind & GDP_nominal<input$less_country)
     dataset <- select(dataset,Number,Country,GDP_nominal_2017,GDP_growth,Pop,GDP_per_capita,Share)
     dataset
   })
   output$total_country <- renderText({
     if (input$country_ind=='All') dataset<-filter(gdp,Country!='All' & GDP_nominal<input$less_country)
-    else dataset <- filter(gdp,Country==input$country_ind & GDP_nominal_2017<input$less_country)
+    else dataset <- filter(gdp,Country==input$country_ind & GDP_nominal<input$less_country)
     dataset <- select(dataset,Number,Country,GDP_nominal_2017,GDP_growth,Pop,GDP_per_capita,Share)
     paste('There are',nrow(dataset),'countries selected')
     })
